dbfb6290c4770ec7518943fa883a1e31669cd5a7
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-04-28 12:15:20 +0200

    Added Greeting test

--- 05/hello_test.go
+++ 05/hello_test.go
@@ -1,9 +1,14 @@
 package hello_test
 
 import (
+	"hello"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-	t.Fatal("not implemented yet")
+	want := "Hello Gophers!"
+	got := hello.Greeting()
+	if got != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
 }
