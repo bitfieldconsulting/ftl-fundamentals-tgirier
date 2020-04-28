9fd33269eedc632c121e585a83d8842322ed484d
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-04-28 14:35:42 +0200

    Added Tests and implemented function

--- 08/calculator_test.go
+++ 08/calculator_test.go
@@ -12,7 +12,14 @@ func TestAdd(t *testing.T) {
 	}{
 		{a: 1, b: 1, want: 2},
 		{a: 2, b: 2, want: 4},
-		{a: 6, b: 7, want: 12},
+		{a: 6, b: 7, want: 13},
+		{a: -1, b: -1, want: -2},
+		{a: 0, b: 1, want: 1},
+		{a: 0, b: 0, want: 0},
+		{a: -1, b: 0, want: -1},
+		{a: 3, b: -2, want: 1},
+		{a: 6, b: 0, want: 6},
+		{a: -3, b: -3, want: -6},
 	}
 	for _, testCase := range testCases {
 		got := calculator.Add(testCase.a, testCase.b)
